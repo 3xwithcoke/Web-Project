a6b34255960ca3765fc64b7f5f8d1d3d4cecd8c3
Suraj Maharjan <suraj.maharjan2058@gmail.com>
2026-03-04 17:07:38 +0545

    ya

--- backend/routes/SkinQuizRoute.js
+++
@@ -1,9 +0,0 @@
-
-const express = require("express").Router();
-const { submitQuiz, getResults, saveSkinQuiz} = require("../controllers/skinquizController");
-
-express.post("/submit", submitQuiz);
-express.get("/results", getResults);
-express.post("/save", saveSkinQuiz);
-
-module.exports = express;
